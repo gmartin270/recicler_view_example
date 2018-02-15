96e31a40c2847e1afc80749f4c0c9d22088a12d7
Guillermo Martín <xguillemartinx@gmail.com>
2018-02-15 17:17:54 -0300

    Implementation example of RecilerView and CardView widgets.

---
+++ app/src/main/java/io/gmartin/reciclerview/activities/MainActivity.java
@@ -0,0 +1,45 @@
+package io.gmartin.reciclerview.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.gmartin.reciclerview.R;
+import io.gmartin.reciclerview.adapters.RVAdapter;
+import io.gmartin.reciclerview.models.Person;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Person> persons;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
+        rv.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+
+
+        initializeData();
+
+        RVAdapter adapter = new RVAdapter(persons);
+        rv.setAdapter(adapter);
+    }
+    // This method creates an ArrayList that has three Person objects
+    // Checkout the project associated with this tutorial on Github if
+    // you want to use the same images.
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.drawable.emma));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.drawable.lavery));
+        persons.add(new Person("Lillie Watts", "35 years old", R.drawable.lillie));
+    }
+}
